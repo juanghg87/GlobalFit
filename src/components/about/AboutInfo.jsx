d1cac66d23223dfeeeef8f85f128e6f53bbe6503
Juan Guillermo Hernández <juanghg@gmail.com>
2025-01-14 10:26:01 -0500

    about section updated, some videos have been changed

--- src/components/about/AboutInfo.jsx
+++ src/components/about/AboutInfo.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import "./aboutInfo.scss";
 import gifgallery1 from '../../Assets/gif-gallery-1.gif';
-import diving from '../../Assets/diving.jpg';
+import petlover from '../../Assets/about-petlover.mp4'
+import deportista from '../../Assets/about-deportista.mp4'
+import entrenadora from '../../Assets/about-entrenadora.mp4'
+import profesora from '../../Assets/about-profesora.mp4'
 
 const info = [
     {
@@ -13,28 +16,28 @@ const info = [
     },
     {
         id: 2,
-        src: gifgallery1,
+        src: profesora,
         title: "Profesora Deportiva",
         info: "Profesional en deportes y rescatista apasionada, con experiencia como profesora de natación en todos los estilos y edades. Comprometida con la seguridad y el aprendizaje, también promuevo habilidades en otros deportes. Amigable y motivadora, inspiro confianza en el agua y fuera de ella, fomentando un estilo de vida activo y saludable.",
 
     },
     {
         id: 3,
-        src: gifgallery1,
+        src: entrenadora,
         title: "Entrenadora Deportiva y Competitiva",
         info: "Entrenadora deportiva con más de 5 años de experiencia en entrenamiento funcional, grupal y rehabilitación. Apasionada por ayudar a otros a alcanzar sus metas, amo trabajar con adultos mayores y personas en recuperación de lesiones deportivas, siempre promoviendo salud, confianza y un enfoque positivo.",
 
     },
     {
         id: 4,
-        src: diving,
+        src: deportista,
         title: "Triatleta",
         info: "Soy una apasionada del deporte y la vida activa. Triatleta dedicada, basketbolista de corazón, y amante de la natación y el buceo. Disfruto superar límites, explorar el mundo bajo el agua, y fomentar estilos de vida saludables con energía, compromiso y una sonrisa siempre presente.",
 
     },
     {
         id: 5,
-        src: gifgallery1,
+        src: petlover,
         title: "¡Mamá perruna de Gato!",
         info: "Soy una mujer apasionada por los animales, dedicada y cariñosa. Mamá perruna de Gato, un peludo que es mi compañero de vida y mi mayor alegría. Amo cuidar, proteger y aprender de ellos, porque cada animal es un mundo lleno de amor incondicional y lecciones únicas..",
 
@@ -54,7 +57,17 @@ const AboutInfo = () => {
             <main className='container'>
                 <div className='info-img-container'>
                     <aside className='img-container-about'>
-                        {selectedInfo && <img src={selectedInfo.src} alt="Selected topic" className='img-selected' />}
+                        {selectedInfo &&
+                            <video src={selectedInfo.src} alt="Selected topic" className='img-selected'
+                                autoPlay
+                                loop
+                                muted
+                                preload="auto"
+                                playsInline
+                            >
+                            </video>
+                            // <img src={selectedInfo.src} alt="Selected topic" className='img-selected' />}
+                        }
                     </aside>
                     <article className='info-container-about'>
                         <span className='professional-name'>Luisa Fernanda Soto</span>
